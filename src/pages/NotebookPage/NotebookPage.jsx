abc271b4081bba404eee96f76f43e7b6f6877bbb
romanamaralit <romana.maralit@gmail.com>
2022-12-08 14:00:44 -0800

    Create form button

--- src/pages/NotebookPage/NotebookPage.jsx
+++ src/pages/NotebookPage/NotebookPage.jsx
@@ -1,7 +1,50 @@
+import {Button, Form} from "react-bootstrap"
+import {useNavigate} from "react-router-dom"
+import {useState, useEffect} from 'react'
+
+
+
 function NotebookPage () {
+    const navigate = useNavigate()
+    const [client, setClient] = useState({
+        name: "",
+        city:"",
+    })
+
+    const handleChange = (event) => {
+        const {name, value} = event.target
+        setClient(prev => {
+            return{
+                ...prev,
+                [name]: value,
+            }
+            })
+        }
+
+    useEffect(() => {
+        console.log(client);
+    }, [client])
+    
+    const handleClick = (event) => {
+        event.preventDefault();
+    }
+
+
     return (
+        <>
         <h1>NotebookPage</h1>
+        <Form>
+            <Form.Group>
+                <Form.Control name='name' value= {client.name} placeholder="Name" onChange={handleChange}/>
+                <Form.Control name='city' value= {client.city} placeholder="City" onChange={handleChange}/>
+                <Button onClick={handleClick}>Add Client</Button>
+            </Form.Group>
+        </Form>
+        <br></br>
+        <Button onClick={() => navigate(-1) }>Home</Button>
+        </>
     )
 }
 
 export default NotebookPage
+  
