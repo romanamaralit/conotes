f19e7c6e89f15b7c0a637b36dec58097caf135a5
romanamaralit <romana.maralit@gmail.com>
2022-12-08 07:49:13 -0800

    Add pages

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(require('./config/checkToken'));
 // Put API routes here, before the "catch all" route
 app.use('/api/users', require('./routes/api/users'));
 
+
 // app.use("/apis/", apisRoutes)
 
 // The following "catch all" route (note the *) is necessary
